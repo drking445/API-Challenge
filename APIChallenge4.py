c756c9629b88cfc3b93574bd5ffb4fc0b697340e
drking445 <metroboomin@Demetriuss-MacBook-Pro-2.local>
2016-11-05 04:18:00 -0400

    API Challenge #4
    Received a dictionary from a son post request and then taking the
    prefix and array we receive from the request and sending back the array
    with the words without the prefix with a post request.

---
+++ APIChallenge4.py
@@ -0,0 +1,32 @@
+##Code2040 Challenge 4
+
+import json
+import requests
+
+
+def call():
+    pay = {'token' : "802ba928cd3ce9acd90595df2853ee2b"}
+    r = requests.post('http://challenge.code2040.org/api/prefix', params=pay)
+    response = r.json()
+    prefix = response['prefix']
+    array = response['array']
+    array2 = []
+    for index in range(len(array)):
+        
+        if not (array[index].startswith(prefix)):
+            array2.append(array[index])
+
+
+ 
+    pay = {'token' : "802ba928cd3ce9acd90595df2853ee2b"}
+    sub = {'array' : array2}
+    req = requests.post('http://challenge.code2040.org/api/prefix/validate', json=sub, params=pay)
+    
+    
+    
+    
+    print(array2)
+    print(array)
+    print(prefix)
+
+call()
