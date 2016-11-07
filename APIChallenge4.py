0d3f1e5fa8bb94d414e8f8dcf983c81d677715f0
drking445 <metroboomin@Demetriuss-MacBook-Pro-2.local>
2016-11-07 13:52:47 -0500

    Changing json post request

--- APIChallenge4.py
+++ APIChallenge4.py
@@ -4,7 +4,6 @@ import json
 import requests
 
 
-
 pay = {'token' : "802ba928cd3ce9acd90595df2853ee2b"}
    
 r = requests.post('http://challenge.code2040.org/api/prefix', json=pay)
@@ -21,16 +20,16 @@ for index in range(len(array)):
         array2.append(array[index])
 
 
-print(array2) 
-requests = {'token' : "802ba928cd3ce9acd90595df2853ee2b", 'array' : array2}
-
-r = requests.post('http://challenge.code2040.org/api/prefix/validate', json=requests)
-    
-    
-    
-    
 print(array2)
-print(array)
-print(prefix)
+
+
+    
+    
+
+pay = {'token' : "802ba928cd3ce9acd90595df2853ee2b", 'array' : array2}
+r = requests.post('http://challenge.code2040.org/api/prefix/validate', json=pay)
+        
+
+print(r)
 
 
