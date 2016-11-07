acde281a55ed9d3acead898d7276319d47520695
drking445 <metroboomin@Demetriuss-MacBook-Pro-2.local>
2016-11-07 12:56:53 -0500

    Post request changed to send json

--- APIChallenge2.py
+++ APIChallenge2.py
@@ -3,8 +3,10 @@ import requests
 import json
 pay = {'token' : "802ba928cd3ce9acd90595df2853ee2b"}
 r = requests.post('http://challenge.code2040.org/api/reverse', params=pay)
+r.headers['Content-Type: application/json']
 response = r.json()
 string = response['string']
 pay = {'token' : "802ba928cd3ce9acd90595df2853ee2b", 'string' : string[::-1]}
 r = requests.post('http://challenge.code2040.org/api/reverse/validate', params=pay)
+r.headers['Content-Type: application/json']
 
--- APIChallenge4.py
+++ APIChallenge4.py
@@ -4,31 +4,33 @@ import json
 import requests
 
 
-def call():
-    pay = {'token' : "802ba928cd3ce9acd90595df2853ee2b"}
+
+pay = {'token' : "802ba928cd3ce9acd90595df2853ee2b"}
    
-    r = requests.post('http://challenge.code2040.org/api/prefix', params=pay)
-    r.headers['Content-Type: application/json']
-    response = r.json()
-    prefix = response['prefix']
-    array = response['array']
-    array2 = []
-    for index in range(len(array)):
+r = requests.post('http://challenge.code2040.org/api/prefix', json=pay)
+  
+response = r.json()
+prefix = response['prefix']
+array = response['array']
+print(response)
+print(prefix)
+array2 = []
+for index in range(len(array)):
         
-        if not (array[index].startswith(prefix)):
-            array2.append(array[index])
+    if not (array[index].startswith(prefix)):
+        array2.append(array[index])
 
 
- 
-    pay = {'token' : "802ba928cd3ce9acd90595df2853ee2b"}
-    sub = {'array' : array2}
-    req = requests.post('http://challenge.code2040.org/api/prefix/validate', json=sub, params=pay)
-    
-    
-    
-    
-    print(array2)
-    print(array)
-    print(prefix)
+print(array2) 
+requests = {'token' : "802ba928cd3ce9acd90595df2853ee2b", 'array' : array2}
 
-call()
+r = requests.post('http://challenge.code2040.org/api/prefix/validate', json=requests)
+    
+    
+    
+    
+print(array2)
+print(array)
+print(prefix)
+
+
