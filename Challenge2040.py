e39dbb97635ee099f0759182845c6470b5a59dab
drking445 <metroboomin@Demetriuss-MacBook-Pro-2.local>
2016-11-07 22:52:59 -0500

    API Challenge #5

--- Challenge2040.py
+++ Challenge2040.py
@@ -5,7 +5,7 @@ import requests
 
 
 pay = {'token' : "802ba928cd3ce9acd90595df2853ee2b"}
-r = requests.post('http://challenge.code2040.org/api/dating', params=pay)
+r = requests.post('http://challenge.code2040.org/api/dating', json=pay)
 response = r.json()
 time = response['datestamp']
 interval = response['interval']
@@ -16,13 +16,15 @@ time2 = datetime.strptime(time, timeformat)
 print(time2)
 times = time2 + timedelta(seconds = interval)
 print(times)
-x = times.isoformat()
+x = str(times.isoformat())
 
 x2 = x
 x3 = x2[:len(x)-7]
 x4 = x3 + 'Z'
-pay = {'token' : "802ba928cd3ce9acd90595df2853ee2b", 'datestamp' : str(x4)}
-r = requests.post('http://challenge.code2040.org/api/dating/validate', params=pay)
+print(x)
+print(x4)
+pay = {'token' : "802ba928cd3ce9acd90595df2853ee2b", 'datestamp' : x4}
+r = requests.post('http://challenge.code2040.org/api/dating/validate', json=pay)
 print(r)
 
 
